1aabd7cc024f808d6244f127b9751869a2d94cb6
Remon Sijrier <remonsijrier@gmail.com>
2024-08-21 10:00:30 +0200

    Dont' paint if pixelcount < 1

--- src/sheetcanvas/AudioClipView.cpp
+++ src/sheetcanvas/AudioClipView.cpp
@@ -116,7 +116,7 @@ void AudioClipView::paint(QPainter* painter, const QStyleOptionGraphicsItem *opt
     //	printf("x, w %f, %f\n", option->exposedRect.x(), option->exposedRect.width());
     qreal xstart = option->exposedRect.x();
     qreal pixelcount = option->exposedRect.width();
-    if (pixelcount < 0.5) {
+    if (pixelcount < 1.0) {
         // apparently this function can be called with no pixelcount to go with
         // so return here nothing to be done
         return;
