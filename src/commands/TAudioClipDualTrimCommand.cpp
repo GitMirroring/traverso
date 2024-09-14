0c537464a086b6b7de12aafae9e577e6f0904ee4
Remon Sijrier <remonsijrier@gmail.com>
2024-09-14 21:52:30 +0200

    Dual Trim: trim nearest edge if there is a before and after audioclip

--- src/commands/TAudioClipDualTrimCommand.cpp
+++ src/commands/TAudioClipDualTrimCommand.cpp
@@ -24,6 +24,7 @@
 #include "TAudioTrack.h"
 
 #include "TContextPointer.h"
+#include "TSheetView.h"
 
 TAudioClipDualTrimCommand::TAudioClipDualTrimCommand(TSheetView* sv, TAudioTrack *audioTrack)
     : TMoveCommand(sv, audioTrack, tr("AudioClip Dual Trim"))
@@ -55,25 +56,58 @@ int TAudioClipDualTrimCommand::undo_action()
 
 int TAudioClipDualTrimCommand::begin_hold()
 {
-    TTimeRef timeRef = cpointer().on_first_input_event_timeref_location();
+    TTimeRef cursorLocation = cpointer().on_first_input_event_timeref_location();
 
-    m_leftAudioClip = m_audioTrack->get_clip_at_location(timeRef);
-    if (!m_leftAudioClip) {
+    TAudioClip* pointedAudioClip = m_audioTrack->get_clip_at_location(cursorLocation);
+    if (!pointedAudioClip) {
         set_canvas_cursor_text(tr("Dual Trim: No AudioClip at this location"));
         return -1;
     }
-    m_rightAudioClip = m_audioTrack->get_audio_clip_after(m_leftAudioClip);
-    if (!m_rightAudioClip) {
-        set_canvas_cursor_text(tr("Dual Trim: No AudioClip after this one"));
-        return -1;
+
+    TAudioClip* audioClipBefore = m_audioTrack->get_audio_clip_before(pointedAudioClip);
+    TAudioClip* audioClipAfter = m_audioTrack->get_audio_clip_after(pointedAudioClip);
+
+    bool leftEdgeCanTrim = false;
+    bool rightEdgeCanTrim = false;
+    bool trimLeftEdge = false;
+    bool trimRightEdge = false;
+    if (audioClipBefore && audioClipBefore->get_location_end() == pointedAudioClip->get_location_start()) {
+        leftEdgeCanTrim = true;
     }
-    if (m_leftAudioClip->get_location()->get_end() != m_rightAudioClip->get_location()->get_start()) {
-        printf("%lld, %lld\n", m_leftAudioClip->get_location()->get_end().universal_frame(), m_rightAudioClip->get_location()->get_start().universal_frame());
-        set_canvas_cursor_text(tr("Dual Trim: Gap between first and second AudioClip"));
+    if (audioClipAfter && audioClipAfter->get_location_start() == pointedAudioClip->get_location_end()) {
+        rightEdgeCanTrim = true;
+    }
+
+    if (leftEdgeCanTrim && rightEdgeCanTrim) {
+        TTimeRef leftEdgeDistance = cursorLocation - pointedAudioClip->get_location_start();
+        TTimeRef rightEdgeDistance = pointedAudioClip->get_location_end() - cursorLocation;
+        if (leftEdgeDistance < rightEdgeDistance) {
+            trimLeftEdge = true;
+        } else {
+            trimRightEdge = true;
+        }
+    } else if (leftEdgeCanTrim) {
+        trimLeftEdge = true;
+    } else if (rightEdgeCanTrim){
+        trimRightEdge = true;
+    }
+
+    if (! (trimLeftEdge || trimRightEdge)) {
+        set_canvas_cursor_text(tr("Dual Trim: No AudioClip before/after this one"));
         return -1;
     }
 
-    m_origLocation = m_newLocation = m_leftAudioClip->get_location()->get_end();
+    if (trimLeftEdge) {
+        m_leftAudioClip = audioClipBefore;
+        m_rightAudioClip = pointedAudioClip;
+    }
+    if (trimRightEdge) {
+        m_leftAudioClip = pointedAudioClip;
+        m_rightAudioClip = audioClipAfter;
+    }
+
+    m_origLocation = m_newLocation = m_leftAudioClip->get_location_end();
+
 
     return 1;
 }
@@ -91,6 +125,8 @@ void TAudioClipDualTrimCommand::cancel_action()
 int TAudioClipDualTrimCommand::jog()
 {
     m_newLocation = cpointer().timeref_location();
+
+    cpointer().set_canvas_cursor_pos(cpointer().on_first_input_event_scene_pos());
 
     do_action();
 
