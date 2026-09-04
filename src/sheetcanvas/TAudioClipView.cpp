1d6fdf715abe8c921972585283b72af62a51f7ce
Ben Levitt <benjie@gmail.com>
2026-09-03 20:03:59 -0700

    Fix showing automation curve for master track

--- src/sheetcanvas/TAudioClipView.cpp
+++ src/sheetcanvas/TAudioClipView.cpp
@@ -217,8 +217,9 @@ void TAudioClipView::draw_peaks(QPainter* p, qreal xstart, int pixelcount)
     // FIXME: find out why?
     if (xstart > 0) {
         xstart -= 1;
-        pixelcount += 2;
+        pixelcount += 1;
     }
+    pixelcount += 1;
 
     if (!peak) {
         //                PERROR("No Peak object available for clip %s", QS_C(m_clip->get_name()));
