b4279e16b32bbfe5d3ffba5a82b3cac67a09ff27
Remon Sijrier <remonsijrier@gmail.com>
2026-09-05 16:24:10 +0200

    only precompile what is useful

--- src/traverso/precompile.h
+++ src/traverso/precompile.h
@@ -1,31 +1,30 @@
-#include <qcoreapplication.h>
-#include <qlist.h>
+#pragma once
 #include <qvariant.h>  // All moc genereated code has this include
-#include <qobject.h>
-#include <qregexp.h>
+#include <QObject>
+#include <QPointer>
 #include <qstring.h>
 #include <qstringlist.h>
-#include <qtextcodec.h>
-
+#include <qdatetime.h>
+#include <QDomNode>
 #include <qapplication.h>
 #include <QMainWindow>
-#include <qbitmap.h>
-#include <qcursor.h>
-#include <qdesktopwidget.h>
-#include <qevent.h>
-#include <qimage.h>
-#include <qlayout.h>
 #include <qpainter.h>
-#include <qpixmap.h>
 #include <qstyle.h>
 #include <qtimer.h>
 #include <qwidget.h>
 #include <QHash>
-#include <QWheelEvent>
+#include <QMap>
 #include <QUndoGroup>
-#include <qgraphicsscene.h>
-#include <qgraphicsview.h>
-#include <qgraphicsitem.h>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsItem>
 #include <QGraphicsSceneMouseEvent>
 #include <QStyleOptionGraphicsItem>
-#include <stdlib.h>
+#include <QGraphicsSceneMouseEvent>
+#include "qassert.h"
+
+#include <cstdio>
+#include <iostream>
+
+#include <chrono>
+
